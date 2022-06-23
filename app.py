099ec880c72116996938fa9a32c572f2bbc43de3
Erin Wills <ejw.data@gmail.com>
2022-06-23 11:28:30 -0500

    updated readme, added ChromeDriverManager

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
+# Dependencies
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
+# instantiate flask
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
+# load index.html and pull content from mongo db
 @app.route("/")
 def index():
     listings = mongo.db.listings.find_one()
     return render_template("index.html", listings=listings)
 
-
+# scrape and store data
 @app.route("/scrape")
 def scraper():
     listings = mongo.db.listings
@@ -21,6 +24,6 @@ def scraper():
     listings.update({}, listings_data, upsert=True)
     return redirect("/", code=302)
 
-
+# run flask
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,24 +1,25 @@
 # Dependencies
+import pandas as pd
 from splinter import Browser
 from bs4 import BeautifulSoup
 import requests
 import time
-import pandas as pd
-
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 
 def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
-    executable_path = {"executable_path": "chromedriver.exe"}
+    executable_path = {'executable_path': ChromeDriverManager().install()}
     return Browser("chrome", **executable_path, headless=False)
 
-
+# This function is used in app.py within the scrape route
 def scrape():
+    
+    #  this function is found within this file at the top
     browser = init_browser()
     listings = {}
 
-    
+    # scrape news page
     url = "https://mars.nasa.gov/news/"
     browser.visit(url)
     time.sleep(1)
@@ -27,7 +28,7 @@ def scrape():
     news_title = soup.find('div', class_='content_title').text
     news_p = soup.find('div', class_='article_teaser_body').text
     
-    
+    # scrape article image
     url_base = "https://www.jpl.nasa.gov"
     url_add = '/spaceimages/?search=&category=Mars'
     browser.visit(url_base+url_add)
@@ -39,8 +40,7 @@ def scrape():
     end=bttn_image_url.find("');")
     featured_image_url=url_base+bttn_image_url[start+3+len("('"):end]
     
-    
-    
+    #scrape twitter for current news
     url_base = "https://twitter.com/marswxreport?lang=en"
     browser.visit(url_base)
     time.sleep(1)
@@ -48,7 +48,7 @@ def scrape():
     soup = BeautifulSoup(html, "html.parser")
     mars_weather = soup.find('p', class_='TweetTextSize').text
     
-    
+    # scrape page for mars table
     url_base = "https://space-facts.com/mars/"
     browser.visit(url_base)
     time.sleep(1)
@@ -58,7 +58,7 @@ def scrape():
     htmltable=table[0].to_html()
     
     
-    
+    # collect multiple images and titles
     link2=[]
     link3=[]
     link4=[]
@@ -79,7 +79,7 @@ def scrape():
         hemisphere_image_urls.append({"title": link4[i], "img_url": link3[i]})
     
     
-    
+    # collect all information into a dictionary
     listings["news_p"] = news_p
     listings["news_title"]=news_title
     listings["featured_image_url"] = featured_image_url
@@ -87,6 +87,7 @@ def scrape():
     listings["html_table"] = htmltable
     listings["hemisphere_img_dict"] = hemisphere_image_urls
 
+    # function returns the dictionary of mars scraped info
     return listings
     
     
